fa48b465decf358d90321ce5983bb20744af26fa
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-27 01:22:14 -0300

    feat/configuracao do ambiente

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// jest.config.js
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+      },
+    collectCoverage: true,
+    collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
+    "!**/jest.config.js"
+    ],
+    preset: "@shelf/jest-mongodb"
+  };
+  
